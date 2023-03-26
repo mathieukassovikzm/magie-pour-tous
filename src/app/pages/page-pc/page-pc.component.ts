406d22b2641ff8802049c0bfac800411b9cd694f
Mathieu le bellec <mathieu.le.bellec@gmail.com>
2023-03-26 12:31:44 +0200

    page-pc&ml + correction style

--- src/app/pages/page-pc/page-pc.component.ts
+++ src/app/pages/page-pc/page-pc.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-pc',
   templateUrl: './page-pc.component.html',
-  styleUrls: ['./page-pc.component.scss'],
 })
 export class PagePcComponent implements OnInit {
   constructor() {}
