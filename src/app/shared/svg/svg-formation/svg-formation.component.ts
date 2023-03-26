f4ab9a39f9cd9228f8f2bb514acfba26c803f3f0
Mathieu le bellec <mathieu.le.bellec@gmail.com>
2023-03-26 17:46:24 +0200

    feat: svg formation

---
+++ src/app/shared/svg/svg-formation/svg-formation.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-formation',
+  templateUrl: './svg-formation.component.html',
+})
+export class SvgFormationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
